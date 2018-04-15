3aecc788ecc03c11ccc23c9aa2d3e83b7a9da7fc
Eduardo Freitas <egoncalvesfreitas@gmail.com>
2018-04-15 11:41:08 -0300

    first version of pixel art project

---
+++ js/designs.js
@@ -0,0 +1,46 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+function makeGrid() {
+    // Your code goes here!
+    event.preventDefault();
+
+    let row= $("#inputHeight");
+    let column = $('#inputWeight'); 
+    let table = $('#pixelCanvas');
+    
+    if ($("tbody").length) $("tbody").remove();
+
+    let tbody = document.createElement("tbody");
+
+    for (let indexRow = 0; indexRow < row.val(); indexRow++) {
+
+        let newTr = document.createElement('tr');
+        
+        for (let indexColumn = 0; indexColumn < column.val(); indexColumn++) {
+            let newtd = document.createElement('td');        
+            newTr.appendChild(newtd);
+        }
+
+        tbody.appendChild(newTr);
+    }
+
+    table.append(tbody);
+    
+}
+
+
+$(function() {
+    $("#sizePicker").submit(makeGrid)
+
+});
+
+
+$('body').on('click', 'td', function() {
+    let color =  $('#colorPicker').val();
+    let colorTd =  $(this).css('background-color');
+
+    $(this).css('background-color', color); 
+    
+});
