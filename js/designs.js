f32a7d01a68cd9d1e3662d7fa49ae7057a589b5e
Eduardo Freitas <egoncalvesfreitas@gmail.com>
2018-04-16 08:14:40 -0300

    Add comments

--- js/designs.js
+++ js/designs.js
@@ -1,15 +1,19 @@
 
 function makeGrid() {
+    //avoid submit behavior 
     event.preventDefault();
-
+    
+    // get input data 
     let row= $("#inputHeight");
     let column = $('#inputWeight'); 
     let table = $('#pixelCanvas');
     
     if ($("tbody").length) $("tbody").remove();
 
+    //
     let tbody = document.createElement("tbody");
 
+      // Draw grid
     for (let indexRow = 0; indexRow < row.val(); indexRow++) {
 
         let newTr = document.createElement('tr');
@@ -28,15 +32,19 @@ function makeGrid() {
 
 
 $(function() {
+    // add listener to select grid size
     $("#sizePicker").submit(makeGrid)
 
 });
 
 
+//fill the pixels
 $('body').on('click', 'td', function() {
+    // set variables
     let color =  $('#colorPicker').val();
     let colorTd =  $(this).css('background-color');
 
+    //
     $(this).css('background-color', color); 
     
 });
